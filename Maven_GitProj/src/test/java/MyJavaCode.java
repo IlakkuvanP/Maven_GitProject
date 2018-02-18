49ad95760a23199253c4bd64eaef757e6d7e7eed
Lakshman <ilakkuvanp@gmail.com>
2018-02-18 23:35:13 +0530

    my first code

---
+++ Maven_GitProj/src/test/java/MyJavaCode.java
@@ -0,0 +1,20 @@
+
+public class MyJavaCode {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("My First Java code understanding");
+		
+		String name = "Ilakkuvan";
+		MyJavaCode m = new MyJavaCode();
+		m.myMethod(name);
+
+	}
+	
+	
+	public void myMethod(String givenname){		
+		System.out.println("It just print name : " + givenname);
+	}
+
+}
