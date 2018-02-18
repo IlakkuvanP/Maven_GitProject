747bcafe8cdf495883d5e1c7793d248ca2d3c0c4
Lakshman <ilakkuvanp@gmail.com>
2018-02-19 00:01:39 +0530

    sdfsdf

--- Maven_GitProj/src/test/java/MyJavaCode.java
+++ Maven_GitProj/src/test/java/MyJavaCode.java
@@ -1,20 +1,19 @@
-
-public class MyJavaCode {
+public class MyJavaCode{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		System.out.println("My First Java code understanding");
 		
-		String name = "Ilakkuvan";
-		MyJavaCode m = new MyJavaCode();
-		m.myMethod(name);
+//		String name = "Ilakkuvan";
+//		MyJavaCode m = new MyJavaCode();
+//		m.myMethod(name);
 
 	}
 	
 	
-	public void myMethod(String givenname){		
-		System.out.println("It just print name : " + givenname);
-	}
+//	public void myMethod(String givenname){		
+//		System.out.println("It just print name : " + givenname);
+//	}
 
 }
